f0dff4a17933ffba30e4b16f0c35e6a853b933be
JavApext <JavApext@gmail.com>
2022-10-31 13:51:24 +0900

    게시판 글보기, 글수정, 파일 스트림

--- JBoard1/src/main/java/kr/co/jboard1/bean/ViewBean.java
+++ JBoard1/src/main/java/kr/co/jboard1/bean/ViewBean.java
@@ -1,6 +1,6 @@
 package kr.co.jboard1.bean;
 
-public class ArticleBean {
+public class ViewBean {
 	private int no;
 	private int parent;
 	private int comment;
@@ -12,39 +12,6 @@ public class ArticleBean {
 	private String uid;
 	private String regip;
 	private String rdate;
-	
-	//추가 필드
-	private String nick;
-	
-	private int fno;
-	private String oriName;
-	private int download;
-	
-	public int getFno() {
-		return fno;
-	}
-	public void setFno(int fno) {
-		this.fno = fno;
-	}
-	public String getOriName() {
-		return oriName;
-	}
-	public void setOriName(String oriName) {
-		this.oriName = oriName;
-	}
-	public int getDownload() {
-		return download;
-	}
-	public void setDownload(int download) {
-		this.download = download;
-	}
-	
-	public String getNick() {
-		return nick;
-	}
-	public void setNick(String nick) {
-		this.nick = nick;
-	}
 	
 	public int getNo() {
 		return no;
@@ -112,5 +79,4 @@ public class ArticleBean {
 	public void setRdate(String rdate) {
 		this.rdate = rdate;
 	}
-
 }
